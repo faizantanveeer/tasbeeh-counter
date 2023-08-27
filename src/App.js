93ba28e200cb0ea1177744fc4b3dae88e945252a
faizantanveeer <ftlegend007@gmail.com>
2023-08-27 14:49:59 +0500

    commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  var [counter, setCounter] = useState(96);
+  var [totalCount, setTotalCount] = useState(0);
+  var [set, setSet] = useState(0);
+
+  var countHandler = () => {
+    totalCount = totalCount + 1;
+    setTotalCount(totalCount);
+    counter = counter + 1;
+    setCounter(counter);
+  };
+
+  var resetHandler = () => {
+    setCounter(0);
+    setTotalCount(0);
+    setSet(0);
+  };
+
+  const limit = 100;
+
+
+  if (counter > limit) {
+    setCounter(0);
+    set = set + 1;
+    setSet(set);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      {/* Navbar */}
+
+      <div className="bg-image"></div>
+      <div className="container">
+        <div className="wrapper">
+          <h1 className="text pb-2">TASBEEH COUNTER</h1>
+          <div className="container m-3">
+            <h1 className="count">{counter}</h1>
+            <p className="limit">/ {limit}</p>
+          </div>
+          <button onClick={countHandler} className="click-btn">
+            Click Here
+          </button>
+          <button onClick={resetHandler} className="reset-btn">
+            Reset
+          </button>
+          <p className="total"> Total Count: <span className="totalNumber">{totalCount}</span></p>
+          <p className="sets">Sets: <span className="totalSets">{set}</span> </p>
+        </div>
+      </div>
     </div>
   );
 }
